f6dc008b2d44ef22e4e3b80da52c3c8c23951779
jonghunbaek <qor6028@naver.com>
2024-07-21 21:03:31 +0900

    refactor : stomp 설정 수정

--- src/main/java/com/example/springwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/example/springwebsocket/config/WebSocketConfig.java
@@ -1,15 +1,18 @@
 package com.example.springwebsocket.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@EnableWebSocketMessageBroker
+@Configuration
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("*");
+        registry.addEndpoint("/websocket");
     }
 
     @Override
