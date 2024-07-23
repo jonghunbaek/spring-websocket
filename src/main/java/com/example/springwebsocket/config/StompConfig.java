ff79c774024912dc74473cfe86f7eba1ccd85c2a
jonghunbaek <qor6028@naver.com>
2024-07-23 17:52:37 +0900

    refactor : 설정 클래스 이름 변경

--- src/main/java/com/example/springwebsocket/config/StompConfig.java
+++ src/main/java/com/example/springwebsocket/config/StompConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @EnableWebSocketMessageBroker
 @Configuration
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
