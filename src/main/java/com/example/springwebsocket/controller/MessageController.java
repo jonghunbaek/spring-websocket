fb42b45db1786a40c91e43514d124434fc743e5e
jonghunbaek <qor6028@naver.com>
2024-07-29 18:54:27 +0900

    feat : 브로커 메세지 전달하도록 추가

--- src/main/java/com/example/springwebsocket/controller/MessageController.java
+++ src/main/java/com/example/springwebsocket/controller/MessageController.java
@@ -1,7 +1,10 @@
 package com.example.springwebsocket.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.stereotype.Controller;
 
 @Slf4j
@@ -9,7 +12,8 @@ import org.springframework.stereotype.Controller;
 public class MessageController {
 
     @MessageMapping("/message")
-    public void publishMessage(String message) {
+    @SendTo("/sub/{chattingRoomId}")
+    public void publishMessage(@DestinationVariable String chattingRoomId,  String message) {
         log.info("message :: {}", message);
     }
 }
