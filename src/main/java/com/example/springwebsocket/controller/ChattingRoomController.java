e2183661d2de21a4005e2e59bbfe37592a653aba
jonghunbaek <qor6028@naver.com>
2024-08-04 17:02:54 +0900

    refactor : 채팅방 입장, 퇴장 API ChattingRoomController로 이동

--- src/main/java/com/example/springwebsocket/controller/ChattingRoomController.java
+++ src/main/java/com/example/springwebsocket/controller/ChattingRoomController.java
@@ -1,9 +1,14 @@
 package com.example.springwebsocket.controller;
 
 import com.example.springwebsocket.controller.dto.ChattingCreationRoomInfo;
+import com.example.springwebsocket.controller.dto.SendMessageRequest;
 import com.example.springwebsocket.service.ChattingRoomService;
 import com.example.springwebsocket.service.dto.ChattingRoomInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +17,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 public class ChattingRoomController {
+
+    private final SimpMessagingTemplate template;
 
     private final ChattingRoomService chattingRoomService;
 
@@ -23,5 +30,21 @@ public class ChattingRoomController {
     @GetMapping
     public List<ChattingRoomInfo> getAllChattingRoom() {
         return chattingRoomService.findAllChattingRooms();
+    }
+
+    @MessageMapping("/entrance/{chattingRoomId")
+    public void enterChattingRoom(@DestinationVariable Long chattingRoomId,
+                                  @Payload SendMessageRequest sendMessageRequest) {
+
+        String enteredMember = chattingRoomService.enterRoom(chattingRoomId, sendMessageRequest.getMemberId());
+        template.convertAndSend("/sub/" + chattingRoomId, enteredMember + "님이 입장 했습니다.");
+    }
+
+    @MessageMapping("/exit/{chattingRoomId")
+    public void exitChattingRoom(@DestinationVariable Long chattingRoomId,
+                                 @Payload SendMessageRequest sendMessageRequest) {
+
+        String exitedMember = chattingRoomService.leaveRoom(chattingRoomId, sendMessageRequest.getMemberId());
+        template.convertAndSend("/sub/" + chattingRoomId, exitedMember + "님이 채팅방을 나갔습니다.");
     }
 }
--- src/main/java/com/example/springwebsocket/service/ChattingRoomService.java
+++ src/main/java/com/example/springwebsocket/service/ChattingRoomService.java
@@ -1,7 +1,11 @@
 package com.example.springwebsocket.service;
 
+import com.example.springwebsocket.entity.Chatting;
 import com.example.springwebsocket.entity.ChattingRoom;
+import com.example.springwebsocket.entity.Member;
+import com.example.springwebsocket.repository.ChattingRepository;
 import com.example.springwebsocket.repository.ChattingRoomRepository;
+import com.example.springwebsocket.repository.MemberRepository;
 import com.example.springwebsocket.service.dto.ChattingRoomInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +21,8 @@ import java.util.List;
 public class ChattingRoomService {
 
     private final ChattingRoomRepository chattingRoomRepository;
+    private final ChattingRepository chattingRepository;
+    private final MemberRepository memberRepository;
 
     public void createChattingRoom(String roomName, int capacity) {
         ChattingRoom chattingRoom = new ChattingRoom(roomName, capacity);
@@ -27,5 +33,37 @@ public class ChattingRoomService {
         return chattingRoomRepository.findAll().stream()
                 .map(ChattingRoomInfo::new)
                 .toList();
+    }
+
+    @Transactional
+    public String enterRoom(Long chattingRoomId, Long memberId) {
+        ChattingRoom chattingRoom = findChattingRoomById(chattingRoomId);
+        Member member = findMemberById(memberId);
+
+        Chatting chatting = Chatting.builder()
+                .member(member)
+                .chattingRoom(chattingRoom)
+                .build();
+
+        chattingRepository.save(chatting);
+
+        return member.getName();
+    }
+
+    public String leaveRoom(Long chattingRoomId, Long memberId) {
+        ChattingRoom chattingRoom = findChattingRoomById(chattingRoomId);
+        Member member = findMemberById(memberId);
+
+        return null;
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하는 회원이 없습니다."));
+    }
+
+    private ChattingRoom findChattingRoomById(Long chattingRoomId) {
+        return chattingRoomRepository.findById(chattingRoomId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하는 채팅방이 없습니다."));
     }
 }
